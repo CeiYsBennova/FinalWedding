647da4ce77cd7a041e048f68f211021eeb0717c6
CeiYs Ekira Loas Bennova <78522909+CeiYsBennova@users.noreply.github.com>
2022-12-05 02:38:10 +0700

    Delete backend directory

--- backend/students/views.py
+++
@@ -1,9 +0,0 @@
-from .models import Student
-from rest_framework.viewsets import ModelViewSet
-from .serializers import StudentSerializer
-
-class StudentsViewSet(ModelViewSet):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-
-
